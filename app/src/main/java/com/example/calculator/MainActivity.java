777698001d7bb4dfcdaebee4f0b0826799a5903d
s860473@gmail.com <Nico@19910320>
2024-03-04 00:12:02 +0800

    test commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//
         number1EditText = findViewById(R.id.number1EditText);
         number2EditText = findViewById(R.id.number2EditText);
         resultTextView = findViewById(R.id.resultTextView);
